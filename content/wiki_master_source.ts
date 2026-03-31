10c271762c5f5ed811a727eb5f5ccc57f43a0c6d
EnzoSim <88380295+EnzoSim@users.noreply.github.com>
2026-03-31 12:33:55 -0400

    Add content/wiki_master_source.ts

---
+++ content/wiki_master_source.ts
@@ -0,0 +1,5 @@
+export const wikiMasterMarkdown = `# Wiki Master
+
+## Urban Economics
+- NIMBYism: An acronym for "Not In My Back Yard," refers to the opposition by residents to proposed real estate, infrastructure, or development projects in their local area, even if they agree with the need for such projects generally. It is a pejorative term used to describe a mindset—often associated with homeowners or established residents—that seeks to protect local property values, neighborhood character, or quality of life at the expense of broader community needs.
+`;
